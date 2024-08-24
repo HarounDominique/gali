7fdeec109e4e6f0cd3eb8538759d9369ef3aea22
Dominique Haroun <haroundominique@gmail.com>
2024-08-25 00:11:46 +0200

    stable version deployed

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,7 @@
 import 'zone.js';
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as L from 'leaflet';
-import { ElevationService } from '../service/elevation-service/elevation.service';
+import {ElevationService} from '../service/elevation-service/elevation.service';
 
 @Component({
   selector: 'app-main',
@@ -15,25 +15,38 @@ export class MainComponent implements OnInit {
   private userLocation: L.LatLng | null = null;
   private targetLocation: L.LatLng | null = null;
   private distanceLine!: L.Polyline;
+  protected selectingUserPosition = false;
+
+  private userPopup!: L.Popup;
+  private targetPopup!: L.Popup;
+  private distancePopup!: L.Popup;
 
   private userIcon = L.icon({
     iconUrl: 'assets/icons/user.png',
-    iconSize: [32, 32],
+    iconSize: [64, 64],
     iconAnchor: [16, 32],
     popupAnchor: [0, -32]
   });
 
   private targetIcon = L.icon({
     iconUrl: 'assets/icons/target.png',
-    iconSize: [32, 32],
+    iconSize: [40, 40],
     iconAnchor: [16, 32],
     popupAnchor: [0, -32]
   });
 
-  constructor(private elevationService: ElevationService) {}
+  currentIcon = this.targetIcon;
+
+  constructor(private elevationService: ElevationService) {
+  }
 
   ngOnInit(): void {
     this.initializeMap();
+  }
+
+  toggleSelection(): void {
+    this.selectingUserPosition = !this.selectingUserPosition;
+    this.currentIcon = this.selectingUserPosition ? this.userIcon : this.targetIcon;
   }
 
   private initializeMap(): void {
@@ -47,27 +60,65 @@ export class MainComponent implements OnInit {
       const lat = event.latlng.lat;
       const lng = event.latlng.lng;
 
-      if (this.targetMarker) {
-        this.targetMarker.setLatLng([lat, lng]);
+      if (this.selectingUserPosition) {
+        this.setUserLocation(lat, lng);
       } else {
-        this.targetMarker = L.marker([lat, lng], { icon: this.targetIcon })
-          .addTo(this.map);
+        this.setTargetLocation(lat, lng);
       }
-      this.targetLocation = L.latLng(lat, lng);
-
-      // Consulta la altitud del destino
-      if (this.targetLocation) {
-        this.elevationService.getElevation(lat, lng).subscribe(elevation => {
-          this.targetMarker
-            .bindPopup(`Altitud del destino: ${elevation} metros`)
-            .openPopup(); // Esto no cierra los popups abiertos, sólo abre el nuevo
-        });
-      }
-
-      this.updateDistance();
     });
 
     this.getUserLocation();
+  }
+
+  private setUserLocation(lat: number, lng: number): void {
+    this.userLocation = L.latLng(lat, lng);
+
+    if (this.userMarker) {
+      this.userMarker.setLatLng([lat, lng]);
+    } else {
+      this.userMarker = L.marker([lat, lng], {icon: this.userIcon}).addTo(this.map);
+    }
+
+    if (this.userLocation) {
+      this.elevationService.getElevation(lat, lng).subscribe(elevation => {
+        if (this.userPopup) {
+          this.userPopup.remove();
+        }
+
+        this.userPopup = L.popup()
+          .setLatLng(this.userLocation!)
+          .setContent(`Altitud del usuario: ${elevation} metros`)
+          .addTo(this.map);
+      });
+    }
+
+    this.map.setView([lat, lng], 13);
+    this.updateDistance();
+  }
+
+  private setTargetLocation(lat: number, lng: number): void {
+    this.targetLocation = L.latLng(lat, lng);
+
+    if (this.targetMarker) {
+      this.targetMarker.setLatLng([lat, lng]);
+    } else {
+      this.targetMarker = L.marker([lat, lng], {icon: this.targetIcon}).addTo(this.map);
+    }
+
+    if (this.targetLocation) {
+      this.elevationService.getElevation(lat, lng).subscribe(elevation => {
+        if (this.targetPopup) {
+          this.targetPopup.remove();
+        }
+
+        this.targetPopup = L.popup()
+          .setLatLng(this.targetLocation!)
+          .setContent(`Altitud del destino: ${elevation} metros`)
+          .addTo(this.map);
+      });
+    }
+
+    this.updateDistance();
   }
 
   private getUserLocation(): void {
@@ -75,31 +126,7 @@ export class MainComponent implements OnInit {
       navigator.geolocation.getCurrentPosition(position => {
         const lat = position.coords.latitude;
         const lng = position.coords.longitude;
-
-        this.userLocation = L.latLng(lat, lng);
-
-        if (this.userMarker) {
-          this.userMarker.setLatLng([lat, lng]);
-        } else {
-          this.userMarker = L.marker([lat, lng], { icon: this.userIcon })
-            .addTo(this.map)
-            .bindPopup('Estás aquí')
-            .openPopup(); // Esto no cierra los popups abiertos, sólo abre el nuevo
-        }
-
-        // Consulta la altitud del usuario
-        if (this.userLocation) {
-          this.elevationService.getElevation(lat, lng).subscribe(elevation => {
-            this.userMarker
-              .bindPopup(`Altitud del usuario: ${elevation} metros`)
-              .openPopup(); // Esto no cierra los popups abiertos, sólo abre el nuevo
-          });
-        }
-
-        this.map.setView([lat, lng], 13);
-
-        // Actualiza la distancia al cargar la ubicación del usuario
-        this.updateDistance();
+        this.setUserLocation(lat, lng);
       }, error => {
         console.error("Error al obtener la ubicación: ", error);
       });
@@ -110,34 +137,57 @@ export class MainComponent implements OnInit {
 
   private updateDistance(): void {
     if (this.userLocation && this.targetLocation) {
-      // Elimina la línea anterior si existe
       if (this.distanceLine) {
         this.map.removeLayer(this.distanceLine);
       }
 
-      // Crea una línea entre los dos puntos
       const latlngs = [this.userLocation, this.targetLocation];
-      this.distanceLine = L.polyline(latlngs, { color: 'blue' }).addTo(this.map);
+      this.distanceLine = L.polyline(latlngs, {color: 'blue'}).addTo(this.map);
 
-      // Calcula la distancia en metros
       const distance = this.userLocation.distanceTo(this.targetLocation);
-
-      // Convierte la distancia a kilómetros si es mayor de 1 km
       const distanceText = distance > 1000
         ? `${(distance / 1000).toFixed(2)} km`
         : `${distance.toFixed(2)} m`;
 
-      // Muestra la distancia en un popup en el medio de la línea
       const midPoint = L.latLng(
         (this.userLocation.lat + this.targetLocation.lat) / 2,
         (this.userLocation.lng + this.targetLocation.lng) / 2
       );
 
-      // Crear un nuevo popup y añadirlo al mapa en la mitad de la línea
-      L.popup()
+      if (this.distancePopup) {
+        this.distancePopup.remove();
+      }
+
+      this.distancePopup = L.popup()
         .setLatLng(midPoint)
         .setContent(`Distancia: ${distanceText}`)
-        .openOn(this.map);
+        .addTo(this.map);
+
+      //Se refresca el popup que muestra la altitud del user:
+      this.elevationService.getElevation(this.userLocation.lat, this.userLocation.lng).subscribe(elevation => {
+        if (this.userPopup) {
+          this.userPopup.remove();
+        }
+
+        this.userPopup = L.popup()
+          .setLatLng(this.userLocation!)
+          .setContent(`Altitud del usuario: ${elevation} metros`)
+          .addTo(this.map);
+      });
+
+      //Se refresca el popup que muestra la altitud del destino:
+      this.elevationService.getElevation(this.targetLocation.lat, this.targetLocation.lng).subscribe(elevation => {
+        if (this.targetPopup) {
+          this.targetPopup.remove();
+        }
+
+        this.targetPopup = L.popup()
+          .setLatLng(this.targetLocation!)
+          .setContent(`Altitud del destino: ${elevation} metros`)
+          .addTo(this.map);
+      });
+
+
     }
   }
 }
